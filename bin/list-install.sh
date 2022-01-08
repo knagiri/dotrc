4e42549cdbc46e5ac0ae59fef820665e42983338
gili-Katagiri <kt.giri1425@gmail.com>
2022-01-08 15:09:55 +0900

    Implement `bin/list-install.sh`

---
+++ bin/list-install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+set -u
+
+commands=( \
+    "git" "docker" "curl"\
+    "rustc" "rustup" "cargo" \
+    "node" "fnm"\
+    "python3" "poetry" \
+    "exa" "bat" "fd" "fzf" \
+)
+
+function is_exist_command {
+    if [ -z "$($1 --version 2> /dev/null)" ]; then
+        return 1
+    fi
+    return 0
+}
+
+out=""
+for _cmd in ${commands[@]}; do
+    is_exist_command ${_cmd} || out="${out} ${_cmd}"
+done
+echo $out
