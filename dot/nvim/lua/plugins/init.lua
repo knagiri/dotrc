7e26b9e4e0ce11897e35e84ee5fd828d71046c26
knagiri <65004703+knagiri@users.noreply.github.com>
2025-03-15 15:51:47 +0900

    feat: ibl and lualine

--- dot/nvim/lua/plugins/init.lua
+++ dot/nvim/lua/plugins/init.lua
@@ -1,9 +1,11 @@
 return {
     require("plugins.fzf"),
     require("plugins.github-copilot"),
-    require("plugins.lsp"),
     require("plugins.neo-tree"),
-    require("plugins.noice"),
+    require("plugins.lsp"),
     require("plugins.treesitter"),
+    require("plugins.noice"),
+    require("plugins.lualine"),
+    require("plugins.indent-blankline"),
     require("plugins.everforest"),
 }
