940f4cebda15aff3e3d7f0e2f7f8173a5967128a
gili-Katagiri <65004703+gili-Katagiri@users.noreply.github.com>
2025-06-22 17:00:07 +0900

    feat: toggleterm and diffview

--- dot/nvim/lua/plugins/init.lua
+++ dot/nvim/lua/plugins/init.lua
@@ -1,8 +1,10 @@
 return {
+    require("plugins.diffview"),
     require("plugins.fzf"),
     -- require("plugins.github-copilot"),
     require("plugins.neo-tree"),
     require("plugins.lsp"),
+    require("plugins.toggleterm"),
     require("plugins.treesitter"),
     require("plugins.noice"),
     require("plugins.lualine"),
